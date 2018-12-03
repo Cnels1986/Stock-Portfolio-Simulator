dffd3d21067ad49450b1515a285bddcd0354615a
Cnels1986 <CNels1986@gmail.com>
2018-12-03 08:37:40 -0500

    instruction popup created, testing has begun...

---
+++ test.py
@@ -0,0 +1,29 @@
+from app import app
+import unittest
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    def test_login_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    def test_register_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/register', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+    def test_logout_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/logout', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+    def test_admin_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/admin', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+    def test_sell_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/sell', content_type='html/text')
+        self.assertEqual(response.status_code, 302)
+
+if __name__ == '__main__':
+    unittest.main()
